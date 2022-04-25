71138b81b2ef7c2d32d1e81729f6d0739058e499
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-04-25 20:03:10 -0300

    update to js.scripts
    added comma's between elements

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,20 @@ let pokemonList = [
     name: 'weedle',
     height: 1,
     type: ['bug', 'poison']
-  }
+  },
   {
     name: 'beedrill',
     height: 3.03,
     type: ['bug', 'poison']
-  }
+  },
   {
     name: 'jigglypuff',
     height: 1.08,
     type: ['fairy', 'balloon']
-  }
+  },
   {
     name: 'weepinbell',
     height: 3.03,
     type: ['grass', 'poison']
-  }
-]
+  },
+];
