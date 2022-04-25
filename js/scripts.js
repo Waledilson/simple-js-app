98470e5338934479fe89e189588b76c39fb20bbe
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-04-25 14:48:11 -0300

    js/scripts additions
    added height to all 4 pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,18 @@
 let pokemonList = [{
   name: 'weedle',
+  height: 1,
   type: ['bug', 'poison']}
   {
     name: 'beedrill',
+    height: 3.03,
     type: ['bug', 'poison']}
     {
       name: 'jigglypuff',
+      height: 1.08,
       type: 'fairy'}
       {
         name: 'weepinbell',
+        height: 3.03,
         type: ['grass', 'poison']
       }
     ]
