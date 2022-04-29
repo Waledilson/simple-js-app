fd51c8716b7bc28c864d965cc6320822313f4a27
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-04-29 11:21:09 -0300

    fixed mistakes
    changed order of pokemon in pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,30 @@
 let pokemonList = [
   {
-    name: 'weedle',
+    name: "Weedle",
     height: 1,
-    type: ['bug', 'poison']
+    type: ["bug","poison"]
   },
-  {
-    name: 'beedrill',
+ {
+    name: "Raticate",
+    height: 2.04,
+    type: ["mouse"]
+  },
+ {
+    name: "Weepinbell",
     height: 3.03,
-    type: ['bug', 'poison']
+    type: ["grass", "poison"]
   },
   {
-    name: 'jigglypuff',
+    name: "Jigglypuff",
     height: 1.08,
-    type: ['fairy', 'balloon']
-  },
-  {
-    name: 'weepinbell',
-    height: 3.03,
-    type: ['grass', 'poison']
-  },
+    type: ["fairy", "balloon"]
+  }
 ];
+  for (let i = 0; i < pokemonList.length; i++)
+    if (pokemonList[i].height < 3)
+    {
+      document.write(pokemonList[i].name + ' ' +  pokemonList[i].height + '<br>');
+  }
+else {
+      document.write(pokemonList[i].name + ' ' + pokemonList[i].height + ' This is a big pokemon!' + '<br>');
+     }
